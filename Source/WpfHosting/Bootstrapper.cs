e20503c3c5f60b77a19e823a71f2d16b0461c379
finphie <19237170+finphie@users.noreply.github.com>
2025-05-03 16:18:06 +0900

    Application.StartupイベントでGeneric Hostを起動するように変更

--- Source/WpfHosting/Bootstrapper.cs
+++ Source/WpfHosting/Bootstrapper.cs
@@ -6,40 +6,30 @@ namespace WpfHosting;
 /// <summary>
 /// WPFアプリケーションのブートストラッパーです。
 /// </summary>
-/// <typeparam name="TApplication">The type of the WPF application.</typeparam>
 /// <typeparam name="TMainWindow">The type of the main window of the WPF application.</typeparam>
-sealed class Bootstrapper<TApplication, TMainWindow> : BackgroundService
-    where TApplication : Application
+sealed class Bootstrapper<TMainWindow> : IHostedService
     where TMainWindow : Window
 {
-    readonly TApplication _application;
     readonly TMainWindow _window;
-    readonly IHostApplicationLifetime _lifetime;
 
     /// <summary>
-    /// <see cref="Bootstrapper{TApplication, TMainWindow}"/>クラスの新しいインスタンスを初期化します。
+    /// <see cref="Bootstrapper{TMainWindow}"/>クラスの新しいインスタンスを初期化します。
     /// </summary>
-    /// <param name="application">WPFアプリケーションのインスタンス</param>
     /// <param name="window">表示するメインウィンドウ</param>
-    /// <param name="lifetime">アプリケーションのライフタイム管理を提供するインターフェイス</param>
-    public Bootstrapper(TApplication application, TMainWindow window, IHostApplicationLifetime lifetime)
+    public Bootstrapper(TMainWindow window)
     {
-        ArgumentNullException.ThrowIfNull(application);
         ArgumentNullException.ThrowIfNull(window);
-        ArgumentNullException.ThrowIfNull(lifetime);
-
-        _application = application;
         _window = window;
-        _lifetime = lifetime;
     }
 
     /// <inheritdoc/>
-    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    public Task StartAsync(CancellationToken cancellationToken)
     {
-        _application.Startup += (_, _) => _window.Show();
-        _application.Exit += (_, _) => _lifetime.StopApplication();
-        _application.Run();
-
+        _window.Show();
         return Task.CompletedTask;
     }
+
+    /// <inheritdoc/>
+    public Task StopAsync(CancellationToken cancellationToken)
+        => Task.CompletedTask;
 }
